cab9beab6dd00d49c09043cc991355ab105baa9d
eqsll <144461926+eqsll@users.noreply.github.com>
2023-12-14 11:09:12 +0300

    Add files via upload

---
+++ задание 2.pas
@@ -0,0 +1,14 @@
+﻿var 
+  f: text;
+  N,K,i: integer;
+begin
+  writeln('Введите N');
+  writeln('Введите K');
+  readln(N);
+  readln(K);
+  assign(f,'C:\Users\eqsll\OneDrive\Рабочий стол\12 лаба\text2.txt');
+  rewrite(f);
+  for i:=1 to N do
+      writeln(f,K*'*');
+  close(f);
+end.
